e663d615d6655c29ed0271f7f51c81d9148b210d
machariamarigi <machariamarigi@gmail.com>
2021-01-14 13:36:01 +0300

    test shallow rendering of App component

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { shallow } from "enzyme";
+import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App", () => {
+  let container
+
+  beforeEach(() => (container = shallow(<App />)))
+
+  it('should render a <div />', () => {
+    expect(container.find('div').length).toEqual(1)
+})
+
 });
