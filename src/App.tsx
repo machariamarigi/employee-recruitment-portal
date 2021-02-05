295de38b883b5d8e876bb11afa2b9ffcf9c9228d
machariamarigi <machariamarigi@gmail.com>
2021-02-05 14:28:12 +0300

    remove App.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,8 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import './App.css';
-import Home from './pages/Home/Home';
-import NotFound from './pages/PageNotFound/NotFound';
 
 const App = () => {
   return (
-    <div className="App">
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route component={NotFound} />
-      </Switch>
-
+    <div>
     </div>
   );
 }
