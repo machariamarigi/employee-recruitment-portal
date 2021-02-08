3647a82e9b881c43464dd6fabae0bbe17cff2a4c
machariamarigi <machariamarigi@gmail.com>
2021-02-08 10:44:55 +0300

    Add a simple Navbar

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <nav className="flex flex-wrap items-center justify-between p-5 bg-blue-800">
+           <p className="text-2xl font-bold text-white">Employee Recruitment Portal</p>
+        </nav>
+    )
+}
