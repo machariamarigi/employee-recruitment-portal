d1055e8b173ca1c975716411db99566ec63df690
machariamarigi <machariamarigi@gmail.com>
2021-02-11 22:10:15 +0300

    Add the home route

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Home from './pages/Home/Home';
 
 const App = () => {
   return (
     <div>
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
     </div>
   );
 }
