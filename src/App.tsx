6516bbb1ef7c177089d4de6a01be0aee87683c0e
machariamarigi <machariamarigi@gmail.com>
2021-02-14 10:33:34 +0300

    add not found component that matches to any undefined route

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from './pages/Home/Home';
+import NotFound from './pages/PageNotFound/NotFound';
 
 const App = () => {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route component={NotFound} />
       </Switch>
     </div>
   );
